259073d19fe6e12fb18fd52f057823fd94ef972c
jakewhinnery <jakewhinnery@gmail.com>
2018-06-18 17:17:37 -0700

    Revised Lab

--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -4,6 +4,6 @@ twoLeapDecade = (365*24*60*8 + 366*24*60*2)
 #number of minutes in a decade assuming that there are three leap years in any given decade
 threeLeapDecade = (365*24*60*7 + 366*24*60*3)
 
-#
+#prints possible answers
 puts ('If there are two leap years in the decade, there will be ' + twoLeapDecade.to_s + ' minutes in the decade.')
 puts ('If there are three leap years in the decade, there will be ' + threeLeapDecade.to_s + ' minutes in the decade.')
