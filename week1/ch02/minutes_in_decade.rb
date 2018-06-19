ce74ff5d5e00fb8f5a6d14efbb04c9fab0d2a5a7
jakewhinnery <jakewhinnery@gmail.com>
2018-06-18 17:15:55 -0700

    Revised Lab

--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -1,2 +1,9 @@
-#number of minutes in a decade assuming that there are two leap years in any given _decade
-puts (365*24*60*8 + 366*24*60*2)
+#number of minutes in a decade assuming that there are two leap years in any given decade
+twoLeapDecade = (365*24*60*8 + 366*24*60*2)
+
+#number of minutes in a decade assuming that there are three leap years in any given decade
+threeLeapDecade = (365*24*60*7 + 366*24*60*3)
+
+#
+puts ('If there are two leap years in the decade, there will be ' + twoLeapDecade.to_s + ' minutes in the decade.')
+puts ('If there are three leap years in the decade, there will be ' + threeLeapDecade.to_s + ' minutes in the decade.')
