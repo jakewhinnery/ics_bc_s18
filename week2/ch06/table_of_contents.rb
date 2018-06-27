a622244ae8926da2e6fc430b2d22d155c804c42b
jakewhinnery <jakewhinnery@gmail.com>
2018-06-26 18:16:44 -0700

    Revised Assignment

--- week2/ch06/table_of_contents.rb
+++ week2/ch06/table_of_contents.rb
@@ -13,12 +13,12 @@ ch1 = left1.ljust(30) + right1.rjust(12)
 #define ch2 with correct spacing
 left2 = 'Chapter 2: Numbers'
 right2 = 'page  9'
-ch2 = left2.ljust(30) + right2.rjust(10)
+ch2 = left2.ljust(30) + right2.rjust(12)
 
 #define ch3 with correct spacing
 left3 = 'Chapter 3: Letters'
 right3 = 'page 13'
-ch3 = left3.ljust(30) + right3.rjust(10)
+ch3 = left3.ljust(30) + right3.rjust(12)
 
 #print the chapters
 puts ch1
