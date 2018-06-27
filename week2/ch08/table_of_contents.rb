3cd78197f71a20fca01d8fd66dd3b624bfdc7f6f
jakewhinnery <jakewhinnery@gmail.com>
2018-06-26 23:06:57 -0700

    Revised Programming Assignment

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,32 +1,21 @@
 #declare line length
-lineLength = 25
+lineLength = 42
+pageNum = 0
 
 #prints title
 title = 'Table of Contents'
-puts title.center(lineLength + 10)
+puts title.center(lineLength)
 
 #declare & initialize array for pages
 pages = ['page  1', 'page  9', 'page 13']
 
 #declare & initialize array for grid
-grid = [['Chapter 1: ', 'Getting Started'], ['Chapter 2: ', 'Numbers'], ['Chapter 3: ', 'Letters']]
+grid = ['Chapter 1: Getting Started', 'Chapter 2: Numbers', 'Chapter 3: Letters']
 
-#declare counters
-x = 0
-y = 0
-
-#increment into different array slots and print the table of contents
-grid.each do |row|
-  string = ""
-  row.each do |col|
-    string += col
-    y += 1
-  end
-  if y == 2
-    puts string + pages[x].rjust(lineLength - 10)
-    x += 1
-  else
-    puts string + pages[x].rjust(lineLength - 4)
-    x += 1
-  end
+#print each line by calling different slots in the arrays
+grid.each do |column|
+  string = ''
+  string = column.ljust(30) + pages[pageNum].rjust(12)
+  puts string
+  pageNum += 1
 end
